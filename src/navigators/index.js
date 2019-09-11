01d7770d9534184400dfc9c109351d5c3acabb7d
toeyanuntachai <toey.anuntachai@gmail.com>
2019-09-11 11:36:13 +0700

    Add register screen

--- src/navigators/index.js
+++ src/navigators/index.js
@@ -3,9 +3,13 @@ import { createStackNavigator } from 'react-navigation-stack';
 import SignInScreen from '../screens/SignInScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import HomeScreen from '../screens/HomeScreen';
+import RegisterScreen from '../screens/RegisterScreen';
 
 const AppStack = createStackNavigator({ Home: HomeScreen });
-const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+const AuthStack = createStackNavigator({
+  SignIn: SignInScreen,
+  Register: RegisterScreen,
+});
 
 export default createAppContainer(
   createSwitchNavigator(
