ccf05b110c6e6306daf4989aeb2ebf6cfdd2b440
toeyanuntachai <toey.anuntachai@gmail.com>
2019-09-13 16:18:34 +0700

    Change app stack to drawer stack

--- src/navigators/index.js
+++ src/navigators/index.js
@@ -1,11 +1,12 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import SignInScreen from '../screens/SignInScreen';
 import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 import HomeScreen from '../screens/HomeScreen';
 import RegisterScreen from '../screens/RegisterScreen';
 
-const AppStack = createStackNavigator({ Home: HomeScreen });
+const AppStack = createDrawerNavigator({ Home: HomeScreen });
 const AuthStack = createStackNavigator({
   SignIn: SignInScreen,
   Register: RegisterScreen,
